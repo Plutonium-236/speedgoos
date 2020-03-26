d5b316be8c0676c64ed2ef2c61c8e641a4a1b3f5
Plutonium-236 <49326963+Plutonium-236@users.noreply.github.com>
2020-03-26 11:43:40 -0700

    done init

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Honcker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Honcker")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("77d7e65e-54ca-41ec-837e-ac07556d49c1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
